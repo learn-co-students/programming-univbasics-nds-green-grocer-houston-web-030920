39f7c98171b743568021f8e15144b5743894b867
Angelo Xenakis <angeloxenakis@gmail.com>
2020-02-18 18:04:23 +0000

    Done.

--- spec/grocer_spec.rb
+++ spec/grocer_spec.rb
@@ -11,7 +11,7 @@ describe "Grocer" do
       {:item => "BEER", :price => 13.00, :clearance => false},
       {:item => "PEANUTBUTTER", :price => 3.00, :clearance => true},
       {:item => "BEETS", :price => 2.50, :clearance => false},
-      {:item => "SOY MILK", :price => 4.50, :clearance => true}
+      {:item => "SOY MILK", :price => 4.50, :clearance => true},
     ]
   end
 
